7a3fa4136eeeda052c83738e4856f1bace40266f
Sanjudhillo <sanjudhillo06@gmail.com>
2025-03-10 19:46:18 +0530

    Add files via upload

---
+++ components/TaskList/TaskList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import AcceptTask from "./AcceptTask";
+import NewTask from "./NewTask";
+import CompleteTask from "./CompleteTask";
+import FailedTask from "./FailedTask";
+
+const TaskList =({data})=>{
+    return(
+        <div id="tasklist" className="h-[55%] overflow-x-auto  flex items-center justify-start  gap-5 flew-nowrap w-full mt-10 py-5">
+           {data.tasks.map((elem ,idx)=>{
+            if(elem.active){
+                return <AcceptTask key={idx} data={elem}/>
+            }if(elem.NewTask){
+                return <NewTask key={idx} data={elem}/>
+            }if(elem.completed){
+                return <CompleteTask key={idx} data={elem}/>
+            }
+            if(elem.FailedTask){
+                return <FailedTask key={idx} data={elem}/>
+            }
+           })}
+            
+        </div>
+    )
+}
+export default TaskList
---
+++ main.jsx
@@ -0,0 +1,18 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import React from'react'
+import AuthProvider from './context/AuthProvider'
+
+createRoot(document.getElementById('root')).render(
+    
+    <React.StrictMode>
+        <AuthProvider>
+         <App />
+        </AuthProvider>
+       
+    </React.StrictMode>,
+  
+)
+
